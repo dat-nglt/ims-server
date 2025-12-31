ac0dd3b18d0d4c06c64669da34212b5cbcb98870
Darren <nguyenletandat.contact@gmail.com>
2025-12-31 08:45:56 +0700

    Xây dựng model lưu trữ thông tin loại chấm công

--- src/models/system/index.js
+++ src/models/system/index.js
@@ -1,3 +1,3 @@
 export { default as SystemConfig } from "./system-config.model.js";
-
+export { default as AttendanceType } from "./attendance-type.model.js";
 export { default as AttendanceLocation } from "./attendance-location.model.js";
